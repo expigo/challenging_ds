c91dbaf2e1b7e334cc4d991d10f5757eba3941b6
Kryspin Musiol <kryspin.musiol@gmail.com>
2025-12-20 02:33:55 +0100

    Fix: file locations

--- src/config.py
+++ src/config.py
@@ -3,5 +3,5 @@ from pathlib import Path
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 
 DATA_DIR = PROJECT_ROOT / "data"
-TRAIN_DATA_PATH = PROJECT_ROOT / "train.parquet"
-TEST_DATA_PATH = PROJECT_ROOT / "test.parquet"
+TRAIN_DATA_PATH = DATA_DIR / "train.parquet"
+TEST_DATA_PATH = DATA_DIR / "test.parquet"
