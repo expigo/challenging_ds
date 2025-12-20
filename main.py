f12945d27f58ab6f6874ff1ce738a4a73983ccc1
Kryspin Musiol <kryspin.musiol@gmail.com>
2025-12-20 02:46:22 +0100

    Feat: add full data loader with Parquet support

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from src.data_loader import load_data
+
 def main():
     print("Hello from ing-task!")
+    train_df, test_df = load_data()
 
 
 if __name__ == "__main__":
--- src/data_loader.py
+++ src/data_loader.py
@@ -1,10 +1,32 @@
 import pandas as pd
 from typing import Tuple
 
-def load_train_data() -> pd.DataFrame:
-    print("Loading training data")
-    train_df = pd.read_parquet('./data/train.parquet')
-    print(train_df.shape)
-    return train_df
+from src.config import TRAIN_DATA_PATH, TEST_DATA_PATH
+
+def load_data() -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Loads the training and test datasets.
+
+    Returns:
+        Tuple[pd.DataFrame, pd.Dataframe]: (train_df, test_df)
+
+    Raises:
+        FileNotFoundError: THe data not found
+    """
+    try:
+        print(f"Loading training data from {TRAIN_DATA_PATH}")
+        train_df = pd.read_parquet(TRAIN_DATA_PATH)
+
+        print(f"Loading test data from {TEST_DATA_PATH}")
+        test_df = pd.read_parquet(TEST_DATA_PATH)
+
+        print(f"Train shape: {train_df.shape}")
+        print(f"Test shape: {test_df.shape}")
+
+    except FileNotFoundError as e:
+        print(f"Error laoding data: {e}")
+        print(f"Please ensure that .parquet files are in the data/ dir")
+
+    return train_df, test_df
 
 
